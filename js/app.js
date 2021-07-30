9dd90782998ed96f511488ce7392f0c911d6860b
Anastasia_Kiryanova <a.kiryanova99@yandex.ru>
2021-07-30 18:02:22 +0500

    error handling

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@
 
 function calculateDepositIncome(amount, periodInMonths) {
     let percent;
-    if(periodInMonths < 6){
+    if (periodInMonths < 6) {
         percent = 2;
-    } else if(periodInMonths >= 6 && periodInMonths < 9){
+    } else if (periodInMonths >= 6 && periodInMonths < 9) {
         percent = 2.2;
-    } else if(periodInMonths >= 9 && periodInMonths < 12){
+    } else if (periodInMonths >= 9 && periodInMonths < 12) {
         percent = 2.3;
-    } else if(periodInMonths >= 12 && periodInMonths < 18){
+    } else if (periodInMonths >= 12 && periodInMonths < 18) {
         percent = 2.6;
-    } else if(periodInMonths >= 18){
+    } else if (periodInMonths >= 18) {
         percent = 2.7;
     }
     const totalAmount = Number((amount * Math.pow(1+((percent/100)/12), periodInMonths)).toFixed());
@@ -26,8 +26,39 @@ function calculateDepositIncome(amount, periodInMonths) {
 function handleSubmit(evt) {
     evt.preventDefault();
 
+    amountErrorEl.textContent = '';
+    periodErrorEl.textContent = '';
+    totalEl.textContent = '';
+    profitEl.textContent = '';
+    percentEl.textContent = '';
+
     const depositAmount = Number(amountInputEl.value);
+    if (Number.isNaN(depositAmount)) {
+        amountErrorEl.textContent = `Неверное значение. Введите число, например: 15000`;
+        return;
+    }
+    if (depositAmount < 15000) {
+        amountErrorEl.textContent = `Неверное значение. Минимальная сумма: 15000 ₽`;
+        return;
+    }
+    if (depositAmount > 50000000) {
+        amountErrorEl.textContent = `Неверное значение. Максимальная сумма: 50000000 ₽`;
+        return;
+    }
+
     const period = Number(periodInputEl.value);
+    if (Number.isNaN(period)) {
+        periodErrorEl.textContent = `Неверное значение. Введите число месяцев, например: 3`;
+        return;
+    }
+    if (period < 3) {
+        periodErrorEl.textContent = `Неверное значение. Минимальный период: 3 месяца`;
+        return;
+    }
+    if (period > 18) {
+        periodErrorEl.textContent = `Неверное значение. Максимальный период: 18 месяцев`;
+        return;
+    }
 
     const result = calculateDepositIncome(depositAmount, period);
     totalEl.textContent = `${result.totalAmount}`;
@@ -45,3 +76,5 @@ const totalEl = document.getElementById('total');
 const profitEl = document.getElementById('profit');
 const percentEl = document.getElementById('percent');
 
+const amountErrorEl = document.getElementById('amount-error');
+const periodErrorEl = document.getElementById('period-error');
