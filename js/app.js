bd48bb11264cb0e56b276aaf6a074958d4a5aefb
Anastasia_Kiryanova <a.kiryanova99@yandex.ru>
2021-07-30 16:16:24 +0500

    function for calculating deposit income

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function calculateDepositIncome(amount, periodInMonth) {
+    let percent;
+    if(periodInMonth < 6){
+        percent = 2;
+    } else if(periodInMonth >= 6 && periodInMonth < 9){
+        percent = 2.2;
+    } else if(periodInMonth >= 9 && periodInMonth < 12){
+        percent = 2.3;
+    } else if(periodInMonth >= 12 && periodInMonth < 18){
+        percent = 2.6;
+    } else if(periodInMonth >= 18){
+        percent = 2.7;
+    }
+    const totalAmount = Number((amount * Math.pow(1+((percent/100)/12), periodInMonth)).toFixed());
+    const profit = totalAmount - amount;
+
+    return {
+        percent,
+        totalAmount,
+        profit,
+    };
+}
+
+
