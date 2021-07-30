70615628d08893bc9a21d0f60c703022cd3ec8c4
Anastasia_Kiryanova <a.kiryanova99@yandex.ru>
2021-07-30 17:12:24 +0500

    handler for the form

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 'use strict';
 
-function calculateDepositIncome(amount, periodInMonth) {
+function calculateDepositIncome(amount, periodInMonths) {
     let percent;
-    if(periodInMonth < 6){
+    if(periodInMonths < 6){
         percent = 2;
-    } else if(periodInMonth >= 6 && periodInMonth < 9){
+    } else if(periodInMonths >= 6 && periodInMonths < 9){
         percent = 2.2;
-    } else if(periodInMonth >= 9 && periodInMonth < 12){
+    } else if(periodInMonths >= 9 && periodInMonths < 12){
         percent = 2.3;
-    } else if(periodInMonth >= 12 && periodInMonth < 18){
+    } else if(periodInMonths >= 12 && periodInMonths < 18){
         percent = 2.6;
-    } else if(periodInMonth >= 18){
+    } else if(periodInMonths >= 18){
         percent = 2.7;
     }
-    const totalAmount = Number((amount * Math.pow(1+((percent/100)/12), periodInMonth)).toFixed());
+    const totalAmount = Number((amount * Math.pow(1+((percent/100)/12), periodInMonths)).toFixed());
     const profit = totalAmount - amount;
 
     return {
@@ -23,4 +23,25 @@ function calculateDepositIncome(amount, periodInMonth) {
     };
 }
 
+function handleSubmit(evt) {
+    evt.preventDefault();
+
+    const depositAmount = Number(amountInputEl.value);
+    const period = Number(periodInputEl.value);
+
+    const result = calculateDepositIncome(depositAmount, period);
+    totalEl.textContent = `${result.totalAmount}`;
+    profitEl.textContent = `${result.profit}`;
+    percentEl.textContent = `${result.percent}`;
+}
+
+const formEl = document.getElementById('deposit-form');
+formEl.addEventListener('submit', handleSubmit);
+
+const amountInputEl = document.getElementById('amount-input');
+const periodInputEl = document.getElementById('period-input');
+
+const totalEl = document.getElementById('total');
+const profitEl = document.getElementById('profit');
+const percentEl = document.getElementById('percent');
 
